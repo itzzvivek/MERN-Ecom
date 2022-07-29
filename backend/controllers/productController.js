5b929dbb6cc4d6cf635d4a44b314971853eeb93b
vivek <vivekwarkade000@gmail.com>
2022-07-29 23:26:04 +0530

    create schema

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,3 +1,17 @@
+const Product = require("../models/productModel");
+
+
+//Create product
+exports.createProduct = async (req,res,next)=>{
+
+    const product = await Product.create(req.body);
+    res.status(201).json({
+        sucess:true,
+        product
+    })
+}
+
+
 exports.getAllProducts = (req , res)=>{
 
     res.status(200).json({message:"Route id working fine"})
--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,11 @@ const connectDatabase = require("./config/database")
 
 dotenv.config({path:"backend/config/config.env"});
 
+//Connecting to database
+connectDatabase()
+
+
+
 app.listen(process.env.PORT,()=>{
 
     console.log(`Server is working on http://localhost:${process.env.PORT}`)
